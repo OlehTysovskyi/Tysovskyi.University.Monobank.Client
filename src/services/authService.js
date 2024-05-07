c14f36a6ca20ee9cdb24b6c4e8794da4dc835f87
Oleh <oleg.tisovsky228@gmail.com>
2024-05-07 11:58:39 +0300

    url changed

--- src/services/authService.js
+++ src/services/authService.js
@@ -6,7 +6,7 @@ export const useAuthentication = () => {
 
   const login = async (formData) => {
     try {
-      const response = await fetch("https://tysovskyi-university-monobank-server.vercel.app/api/login", {
+      const response = await fetch("/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
