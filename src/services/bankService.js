c63ae5375ff79468aa62aa94799284bd34a165a5
Oleh Tysovskyi <oleg.tisovsky228@gmail.com>
2024-05-18 15:19:56 +0300

    rfwrwrw

--- src/services/bankService.js
+++ src/services/bankService.js
@@ -44,6 +44,7 @@ export const useBankService = () => {
       }
 
       const bankData = await response.json();
+      console.error("bank data: " + bankData + " | " + bankData.bank);
       return bankData.bank;
     } catch (error) {
       console.error("Error fetching bank data:", error);
