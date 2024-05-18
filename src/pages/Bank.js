333ce61b04a0ec464f1851ff1e22fcad93c09fdb
Oleh Tysovskyi <oleg.tisovsky228@gmail.com>
2024-05-18 14:48:49 +0300

    added bank data from server

--- src/pages/Bank.js
+++ src/pages/Bank.js
@@ -1,38 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink, useParams } from "react-router-dom";
+import { useBankService } from "../services/bankService";
 import BackButton from "../components/BackButton";
 import bank from "../assets/images/bank.jpg";
 
 const Bank = () => {
   const { id } = useParams();
+  const { getBankData } = useBankService();
+  const [bankData, setBankData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getBankData(id);
+        setBankData(data);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+
+    fetchData();
+  }, [id]);
 
   return (
     <div className="bank">
       <BackButton to="/savings" color="white" />
       <div className="bank-container">
         <div className="info">
-          <div className="name">На машину{id}</div>
+          <div className="name">{bankData.name}</div>
           <div className="divider"></div>
-          <div className="balance">1500{} ₴</div>
+          <div className="balance">{bankData.balance} ₴</div>
           <div className="statistics">
-            Знято{1500} ₴ | Поповнено {2000} ₴
+            Знято {bankData.withdraw_amount} ₴ | Поповнено {bankData.deposit_amount} ₴
           </div>
         </div>
 
-        <img src={bank} alt="cat"></img>
+        <img src={bank} alt="bank" />
 
         <div className="options">
-          <NavLink>
+          <NavLink to="/deposit">
             <div className="btn">+</div>
             <p>Поповнити</p>
             <p>банку</p>
           </NavLink>
-          <NavLink>
+          <NavLink to="/withdraw">
             <div className="btn">-</div>
             <p>Зняти</p>
             <p>з банки</p>
           </NavLink>
-          <NavLink>
+          <NavLink to="/break">
             <div className="btn">||</div>
             <p>Розбити</p>
             <p>банку</p>
